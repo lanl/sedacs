54396f7640e5337b1b6a68dc2012b0823b57cb25
cnegre <christianfannegre@gmail.com>
2023-11-20 13:49:19 -0700

    added nl scaling script

---
+++ driver/run_nl_times.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+#Make sure to source the environmental variable 
+#sour vars
+
+sdc_input=$'Threshold= 1.0E-5 \n CoordsFile= \n Rcut= 5.0 '
+
+export OMP_NUM_THREADS=1
+
+run="python sdc_main_nl_test.py"
+
+tag="Time for build_nlist" 
+
+fileout="times_nl.dat"
+rm $fileout 
+
+for numranks in 1 2 4 
+do
+  echo " "${numranks}  >> $fileout
+  echo "#"${numranks}  >> $fileout
+  for coordsSize in 5133  10000 20000 30000 65000 
+  do
+    echo "coords, numranks:" "$coordsSize" "$numranks"
+    coordsFile="coords_"$coordsSize".pdb"
+    echo "$sdc_input" > tmp
+    echo "$sdc_input" 
+    sed 's/CoordsFile=.*/CoordsFile= '$coordsFile'/g' tmp > input.in
+    /usr/bin/mpirun -np $numranks /usr/bin/python3 sdc_main_nl_test.py | tee  out$coordsSize$numranks
+    time=`grep -e $tag out$coordsSize$numranks | head -1 | awk '{print $4}'`
+    echo $coordsSize $time >> $fileout
+  done
+done
+
---
+++ driver/sdc_main_nl_test.py
@@ -0,0 +1,65 @@
+#!/usr/bin/env python3
+""" Main sedacs prototype driver
+
+"""
+
+import argparse
+from sdc_parser import *
+from sdc_system import *
+from proxy_a import *
+import time
+try:
+    from mpi4py import MPI
+    mpi = True
+except ImportError as e:
+    mpi = False
+from sdc_graph import *
+from sdc_partition import *
+
+
+parser = argparse.ArgumentParser(description='Test driver for sedacs')
+
+parser.add_argument("--use-torch",help="Use pytorch",required=False,action="store_true")
+    
+args=parser.parse_args()
+if args.use_torch:
+    try:
+        import torch as tc
+        if tc.cuda.is_available():
+            print("Using CUDA")
+            args.device = tc.device('cuda')
+        elif tc.backends.mps.is_available():
+            print("Using MPS")
+            args.device = tc.device('mps')
+        else:
+            args.device = tc.device('cpu')
+        from sdc_torch import *
+    except ImportError as e:
+        raise ImportError("Unable to import pytorch")
+            
+comm = MPI.COMM_WORLD
+rank = comm.Get_rank()
+numranks = comm.Get_size()
+
+#Initialize the code by reading the input file
+sdc = sdc_input("input.in",True)
+
+#Read the coordinates
+sy = system(1)
+sy.latticeVectors,sy.symbols,sy.types,sy.coords = \
+    read_coords_file(sdc.coordsFileName,lib="None",verb=True)
+sy.nats = len(sy.coords[:,0])
+
+tic = time.perf_counter()
+if args.use_torch:
+    nl = build_nlist_torch(sy.coords,sy.latticeVectors,5.0,rank=rank,numranks=numranks,verb=False)
+else:    
+    nl,nlTrX,nlTrY,nlTrZ = build_nlist(sy.coords,sy.latticeVectors,5.0,rank=rank,numranks=numranks,verb=False)
+comm.Barrier()
+toc = time.perf_counter()
+print("Time for build_nlist", toc - tic,"(s)")
+if rank == 0:
+    with open('neighborinfo.txt','w') as of:
+        for kk in range(sy.nats):
+            print("Neighs (x-coords) of {} = ".format(kk),nl[kk,1:nl[kk,0]],"(",sy.coords[nl[kk,1:nl[kk,0]],0],")",file=of)
+exit(0)
