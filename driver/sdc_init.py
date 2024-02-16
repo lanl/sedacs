fa6bf74e2b577d1b16f6c451860f247348b83e2d
cnegre <christianfannegre@gmail.com>
2024-02-16 11:19:11 -0700

    commenting lines

--- driver/sdc_init.py
+++ driver/sdc_init.py
@@ -95,6 +95,7 @@ def init(args):
     #subSy.coords,subSy.types = extract_subsystem(sy.coords,sy.types,sy.symbols,nl[1234,1:nl[1234,0]])
     #if rank == 0:
     #    write_pdb_coordinates("subSyNL.pdb",subSy.coords,subSy.types,subSy.symbols)
+    #    exit(0)
 
     #Get initial graph (from a neighbor list)
     graphNL = get_initial_graph(sy.coords,nl,sdc.rcut,sdc.maxDeg,True)
