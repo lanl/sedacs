54931e15f3da0037ba2ff0c5c229e2e8c574c7b2
Joshua David Finkelstein - 350854 <finkeljos@gmail.com>
2024-02-25 20:40:31 -0700

    committing dnnsp2 edits

--- gpu/nvda/include/dnnsp2.cuh
+++ gpu/nvda/include/dnnsp2.cuh
@@ -1,20 +1,9 @@
 
-/*typedef enum{
-    yes       = 0,
-    no        = 1   
-} refine_t; 
-
-typedef enum{
-    fp64      = 0,
-    fp32      = 1, 
-    fp16_fp32 = 2   
-} precision_t; 
-*/
 
 void dnnsp2(double*, 
             double*, 
             int, 
             int,
-            precision_t,
-            refine_t);
+            precision_t);//,
+//            refine_t);
 
