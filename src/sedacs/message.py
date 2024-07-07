5a7b275a60598a7e3b8ad8b5c791a6a2732178e9
singularitti <singularitti@outlook.com>
2024-07-07 14:45:30 -0600

    Fix src/sedacs/message.py

--- src/sedacs/message.py
+++ src/sedacs/message.py
@@ -5,8 +5,10 @@ This will provide some standard messages used throughout the code
 
 import sys
 
+__all__ = ["sdc_status_at", "sdc_error_at", "sdc_warning_at", "sdc_fail_at", "sdc_test_fail", "sdc_test_pass"]
 
-class textcolor:
+
+class TextColor:
     green = "\033[92m"
     yellow = "\033[93m"
     red = "\033[91m"
@@ -21,21 +23,21 @@ def sdc_error_at(name, message=None):
     print("\n !!!ERROR at", name, "\n")
     if message != None:
         print("    ", message, "\n")
-    exit(0)
+    sys.exit(0)
 
 
 def sdc_warning_at(name):
     print("\n !!!WARNING at", name, "\n")
 
 
+def sdc_fail_at(name):
+    print("\n !!!ERROR at", name, "\n")
+    sys.exit(1)
+
+
 def sdc_test_fail(name):
-    print("  Test for ", name, "... " + textcolor.red + "Failed" + textcolor.normal)
+    print("  Test for ", name, "... " + TextColor.red + "Failed" + TextColor.normal)
 
 
 def sdc_test_pass(name):
-    print("  Test for ", name, "... " + textcolor.green + "Passed" + textcolor.normal)
-
-
-def sdc_fail_at(name):
-    print("\n !!!ERROR at", name, "\n")
-    exit(1)
+    print("  Test for ", name, "... " + TextColor.green + "Passed" + TextColor.normal)
