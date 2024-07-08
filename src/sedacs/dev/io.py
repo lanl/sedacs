bbb71efacc16174578542cb676e5f907633da5a1
singularitti <singularitti@outlook.com>
2024-07-08 14:48:30 -0600

    Add `project_path` & `bindeps_path` in src/sedacs/dev/io.py

--- src/sedacs/dev/io.py
+++ src/sedacs/dev/io.py
@@ -2,7 +2,7 @@ import importlib.util
 import os.path
 from pathlib import Path
 
-__all__ = ["src_path", "data_path", "plots_path", "examples_path"]
+__all__ = ["project_path", "src_path", "bindeps_path", "data_path", "plots_path", "examples_path"]
 
 
 def locate_package(package_name):
@@ -20,13 +20,21 @@ def src_path():
     return locate_package("sedacs")
 
 
+def project_path():
+    return src_path().parent.parent
+
+
 def data_path():
-    return os.path.join(src_path(), "data")
+    return project_path() / "data/"
 
 
 def plots_path():
-    return os.path.join(src_path(), "plots")
+    return project_path() / "plots/"
 
 
 def examples_path():
-    return os.path.join(src_path(), "examples")
+    return project_path() / "examples/"
+
+
+def bindeps_path():
+    return project_path() / "bindeps/"
