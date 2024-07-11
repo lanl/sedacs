2690d2ddcbb4adb62708218e2ff1c0833b063e74
singularitti <singularitti@outlook.com>
2024-07-11 11:51:42 -0600

    Fix examples/graph_adaptive_dm/sdc_driver.py → examples/graph_adaptive_dm/main.py

--- examples/graph_adaptive_dm/main.py
+++ examples/graph_adaptive_dm/main.py
@@ -1,6 +1,10 @@
 """Main sedacs prototype driver"""
 
-from sdc_loadMods import *
+import sys
+
+import numpy as np
+from sedacs.driver.graph_adaptive import get_adaptiveDM
+from sedacs.driver.init import get_args, init
 
 # Pass arguments from comand line
 args = get_args()
@@ -16,4 +20,4 @@ sdc.verb = True
 print("!!!", graphNL[0], graphNL.shape)
 
 # Perform a graph-adaptive calculation of the density matrix
-get_adaptiveDM(sdc,eng,comm,rank,numranks,sy,hindex,graphNL)
+get_adaptiveDM(sdc, eng, comm, rank, numranks, sy, hindex, graphNL)
