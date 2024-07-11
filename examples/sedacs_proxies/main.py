4d5e9642368bb217f666d71b38b82fe9c0ef4034
singularitti <singularitti@outlook.com>
2024-07-11 14:53:09 -0600

    Reformat examples/sedacs_proxies/main.py

--- examples/sedacs_proxies/main.py
+++ examples/sedacs_proxies/main.py
@@ -1,15 +1,15 @@
-import numpy as np
-from sedacs.system import *
-from proxy_a import *
+from sedacs.io import read_pdb_file, write_xyz_coordinates
 from sedacs.periodic_table import PeriodicTable
 
+from proxies.python.first_level import get_hamiltonian
+
 pt = PeriodicTable()
-latticeVectors,symbols,types,coords = read_pdb_file("coords.pdb",lib="None",verb=True)
-nats = len(coords[:,1])
-write_xyz_coordinates(coords,types,symbols)
+latticeVectors, symbols, types, coords = read_pdb_file("coords.pdb", lib="None", verb=True)
+nats = len(coords[:, 1])
+write_xyz_coordinates(coords, types, symbols)
 H = get_hamiltonian(coords)
-N = len(H[:,1])
-Nocc = int(N/2)
-D = get_densityMatrix(H,Nocc)
-print("Hamiltonian = ",H)
-print("Density Matrix = ",D)
+N = len(H[:, 1])
+Nocc = int(N / 2)
+D = get_densityMatrix(H, Nocc)
+print("Hamiltonian = ", H)
+print("Density Matrix = ", D)
