c53c252cb62b7d959b3f4e0649ef4a5d394d5c5e
singularitti <singularitti@outlook.com>
2024-08-13 13:14:15 -0600

    Reformat Julia code

--- src/sedacs/gpu/jul/GenSP2/src/GenSP2.jl
+++ src/sedacs/gpu/jul/GenSP2/src/GenSP2.jl
@@ -3,6 +3,14 @@ module GenSP2
 include("training.jl")
 include("inference.jl")
 
-export read_or_generate_models, generate_model, energy_fn, fermi_fn, entropy_fn, model_entropy, model_fermi, sample_weights, layer_width
+export read_or_generate_models,
+    generate_model,
+    energy_fn,
+    fermi_fn,
+    entropy_fn,
+    model_entropy,
+    model_fermi,
+    sample_weights,
+    layer_width
 
 end # module
--- src/sedacs/gpu/jul/GenSP2/src/inference.jl
+++ src/sedacs/gpu/jul/GenSP2/src/inference.jl
@@ -1,10 +1,9 @@
 using LinearAlgebra
 using SparseArrays
 
-
 function matrix_fn(f, a)
     e = eigen(a)
-    e.vectors * diagm(f.(e.values)) * inv(e.vectors)
+    return e.vectors * diagm(f.(e.values)) * inv(e.vectors)
 end
 
 function heaviside_matrix(x, θ)
@@ -14,9 +13,9 @@ function heaviside_matrix(x, θ)
 
     y = x
     Y = zero(x)
-    for i = 1:nlayers
+    for i in 1:nlayers
         Y += θ[4, i] * y
-        y = θ[1, i] * y^2 + θ[2, i] * y + θ[3, i]*I
+        y = θ[1, i] * y^2 + θ[2, i] * y + θ[3, i] * I
     end
     Y += y
     return Y
@@ -27,7 +26,6 @@ function fermi_matrix(x, θ)
     return I - Y
 end
 
-
 function heaviside_matrix!(res, temp1, temp2, x, θ)
     npts = length(x)
     n = size(x, 2)
@@ -35,12 +33,12 @@ function heaviside_matrix!(res, temp1, temp2, x, θ)
     θ = reshape(θ, layer_width, :)
     nlayers = size(θ, 2)
 
-    fill!(res, 0.)
+    fill!(res, 0.0)
     y = temp1
     y² = temp2
     copy!(y, x)
 
-    for i = 1:nlayers
+    for i in 1:nlayers
         BLAS.gemm!('N', 'N', one(typ), y, y, zero(typ), y²) # y² = y*y
 
         BLAS.axpy!(θ[4, i], y, res)
@@ -49,7 +47,7 @@ function heaviside_matrix!(res, temp1, temp2, x, θ)
         BLAS.axpby!(θ[1, i], y², θ[2, i], y) # y = θ₁ y² + θ₂ y
         # @. y = θ[1, i] * y² + θ[2, i] * y
 
-        y[1:n+1:n*n] .+= θ[3, i] # y += θ₃ I
+        y[1:(n + 1):(n * n)] .+= θ[3, i] # y += θ₃ I
     end
 
     BLAS.axpy!(1.0, y, res)
@@ -62,8 +60,8 @@ function fermi_matrix!(res, temp1, temp2, x, θ)
     heaviside_matrix!(res, temp1, temp2, x, θ)
 
     n = size(x, 2)
-    BLAS.scal!(n*n, -1., res, 1) # res *= -1
-    res[1:n+1:n*n] .+= 1.0       # res += I
+    BLAS.scal!(n * n, -1.0, res, 1) # res *= -1
+    res[1:(n + 1):(n * n)] .+= 1.0       # res += I
 
     return res
 end
