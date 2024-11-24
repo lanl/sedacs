c583d2aaf38b556d6a6be62ac1f7b47bc7b2f6e1
kulichenko-LANL <maxim@lanl.gov>
2024-11-23 23:02:59 -0700

    odd electron fix

--- src/sedacs/driver/graph_adaptive.py
+++ src/sedacs/driver/graph_adaptive.py
@@ -127,7 +127,7 @@ def get_singlePoint(sdc, eng, rank, node_rank, numranks, comm, parts, partsCoreH
         print("| t eVals/dVals {:>9.4f} (s)".format(time.perf_counter() - tic))
 
     Q_list_np = [tensor.cpu().numpy() for tensor in Q_list]
-    #Q_list_np = [arr.astype(np.float32) for arr in Q_list_np]
+    Q_list_np = [arr.astype(np.float32) for arr in Q_list_np]
 
 
     comm.Barrier()
@@ -137,8 +137,8 @@ def get_singlePoint(sdc, eng, rank, node_rank, numranks, comm, parts, partsCoreH
     eValOnRank_size = np.array(len(eValOnRank), dtype=int)
     eValOnRank_SIZES = None
 
-    buf_size = 2**31  # 2 GB buffer (adjust as needed)
-    MPI.Attach_buffer(bytearray(buf_size))
+    # buf_size = 2**31  # 2 GB buffer (adjust as needed)
+    # MPI.Attach_buffer(bytearray(buf_size))
 
     if mpiOnDebugFlag:
         if rank == 0:
@@ -165,7 +165,7 @@ def get_singlePoint(sdc, eng, rank, node_rank, numranks, comm, parts, partsCoreH
             # Flatten the nested list of lists into a single list of tensors
             eVal_LIST = list(itertools.chain(*eVal_LIST))
             Q_LIST = list(itertools.chain(*Q_LIST))
-            Q_LIST = [torch.from_numpy(arr) for arr in Q_LIST]
+            Q_LIST = [torch.from_numpy(arr).to(dtype=torch.float64) for arr in Q_LIST]
             NH_Nh_Hs_LIST = list(itertools.chain(*NH_Nh_Hs_LIST))
             I_LIST = list(itertools.chain(*I_LIST))
             I_halo_LIST = list(itertools.chain(*I_halo_LIST))
@@ -183,7 +183,7 @@ def get_singlePoint(sdc, eng, rank, node_rank, numranks, comm, parts, partsCoreH
         core_indices_in_sub_expanded_LIST = core_indices_in_sub_expanded_list
         Nocc_LIST = Nocc_list
 
-    MPI.Detach_buffer()
+    #MPI.Detach_buffer()
 
     if node_rank == 0: print("| t commLists {:>9.4f} (s)".format(time.perf_counter() - tic), rank)
     if rank == 0:
