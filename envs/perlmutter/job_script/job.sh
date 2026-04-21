0564ce301ca68247d331e5d47d7d8930dd63293c
Cheng-Han Li <58865377+peterli3819@users.noreply.github.com>
2026-04-21 14:47:44 -0600

    Update LD_LIBRARY_PATH for qmd-progress installation

--- envs/perlmutter/job_script/job.sh
+++ envs/perlmutter/job_script/job.sh
@@ -15,7 +15,7 @@ module load python
 mamba activate $HOME/mamba_sedacs 
 export LATTE_PATH=~/LATTE/
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:~/bml/install/lib64/
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:~/bml/install/lib64/
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:~/qmd-progress/install/lib64/
 export OMP_PROC_BIND=spread
 export OMP_PLACES=cores
 export OMP_NUM_THREADS=4
