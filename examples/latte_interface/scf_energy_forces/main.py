d3aec699cdd280193f44129235e1d83ae27235bd
peterli3819 <peterli3819@gmail.com>
2026-04-30 11:45:44 -0600

    update scf example for sedacs-latte interface

--- examples/latte_interface/scf_energy_forces/main.py
+++ examples/latte_interface/scf_energy_forces/main.py
@@ -12,12 +12,10 @@ of the energy and forces.
 import sys
 
 import numpy as np
-from sedacs.driver.graph_adaptive_scf import get_adaptiveSCFDM
+from sedacs.driver.graph_adaptive_kernel_scf import get_adaptive_KernelSCFDM 
 from sedacs.driver.graph_adaptive_sp_energy_forces import get_adaptive_sp_energy_forces
 from sedacs.driver.init import get_args, init
 from sedacs.file_io import read_latte_tbparams
-from sedacs.charges import get_charges
-import sedacs.globals as gl
 
 
 # Pass arguments from command line
@@ -28,7 +26,7 @@ args = get_args()
 np.set_printoptions(threshold=sys.maxsize)
 
 # Initialize sdc parameters
-sdc, eng, comm, rank, numranks, sy, hindex, graphNL, graphweights, nl, nlTrX, nlTrY, nlTrZ = init(
+sdc, eng, comm, rank, numranks, sy, hindex, graphNL, graphweights = init(
     args
 )
 
@@ -38,6 +36,7 @@ sdc.verb = True
 latte_tbparams = read_latte_tbparams(
     "../../../parameters/latte/TBparam/electrons.dat"
 )
+
 # Get the Hubbard U values for each atom in the system
 Hubbard_U = [latte_tbparams[symbol]["HubbardU"] for symbol in sy.symbols]
 Hubbard_U = np.array(Hubbard_U)[sy.types]
@@ -45,12 +44,13 @@ sy.hubbard_u = Hubbard_U
 
 # Perform a graph-adaptive calculation of the density matrix through SCF cycles
 mu = 0.0
-graphDH, sy.charges, mu, parts, partsCoreHalo, subSysOnRank = get_adaptiveSCFDM(
-    sdc, eng, comm, rank, numranks, sy, hindex, graphNL, mu, graphweights=graphweights
+graphDH, sy.charges, mu, parts, partsCoreHalo, subSysOnRank = get_adaptive_KernelSCFDM(
+    sdc, eng, comm, rank, numranks, sy, hindex, graphNL, mu, graphweights=graphweights,
 )
 # Perform a single-point graph-adaptive calculation of the energy and forces
-graphDH, sy.charges, energy, forces, mu, parts, partsCoreHalo, subSysOnRank = get_adaptive_sp_energy_forces(
-    sdc, eng, comm, rank, numranks, sy, parts, partsCoreHalo, hindex, graphNL, mu
+graphDH, sy.charges, energy, entropy, forces, mu, parts, partsCoreHalo, subSysOnRank = get_adaptive_sp_energy_forces(
+    sdc, eng, comm, rank, numranks, sy, parts, partsCoreHalo, hindex, graphDH, mu
 )
+
 print("total energy:", energy)
 print("forces:", forces[0])
